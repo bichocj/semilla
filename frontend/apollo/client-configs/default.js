1098223b04140b719e92d2f88504b71f24929c45
bichocj <bichocj@gmail.com>
2019-05-18 04:31:42 -0500

    update service path

--- frontend/apollo/client-configs/default.js
+++ frontend/apollo/client-configs/default.js
@@ -3,7 +3,7 @@ import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 // Replace this with your project's endpoint
-const GRAPHCMS_API = 'http://localhost:3000/graphql'
+const GRAPHCMS_API = process.env.NODE_ENV === 'production' ? '/graphql' : 'http://localhost:3000/graphql'
 
 export default () => ({
   link: new HttpLink({ uri: GRAPHCMS_API }),
